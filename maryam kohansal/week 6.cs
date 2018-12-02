2e711200ff8d509481ad5b1cf46d27f458239c20
mahdihadian <mahdihadian@hotmail.com>
2018-12-02 23:46:41 +0330

    10azar 11:47PM

---
+++ maryam kohansal/week 6.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10اذر
+{
+    public class employee
+
+    { 
+               private int id, int hiredate;
+                privait string name, stringlastname,string degree;
+
+        //سازنده یا متد
+        public employee()
+        {
+        }
+        public employee(int idvalue, int hiredate..value,string name,string lastname,string degree)
+        {
+            id= id value;
+
+            hiredate= hiredate value;
+            name = namespace Value;
+            lastname = lastname value;
+            degree = degree value;
+    }
+
+                public int id
+            {
+                get
+                {
+                    return id;
+                }
+                set {
+                   id =value;
+                }
+                public int hiredate
+                {
+                    get{
+                        return hiredate;
+                    }
+                    set{
+                        date =value;
+                    }
+                    public string name
+                    {
+                        get{
+                            return name;
+                        }
+                        set{
+                            name = value;
+                        }
+                        public string lastname 
+                        {
+                            get{
+                                return lastname;
+                            }
+                            set{
+                                lastname = value
+                            }
+                  }
+
+                
+                private int id, int hiredate ;
+                privait string name, stringlastname,string degree educaition;
+
+        //سازنده یا متد
+        public employee()
+        {
+        }
+        public employee(int idvalue, int hiredate..value,string name,string lastnam, string degree)
+        {
+            id= id value;
+
+            hiredate= date value;
+
+            public int id
+            {
+                get
+
+             public class H.employee : employee
+                 {
+                     private int max act ,int salary;
+
+                    public H.employee()
+              {
+                 }
+                     public H.employee(int id, int hiredate, string name, string lastname,string degree,int max act, int salary)
+             {
+
+                id= id value;
+
+            date= date value;
+            name = name Value;
+            lastname = lastname value;
+            degree = degree value;
+             max act= max act value;
+             salary = salary value;
+             }
+            
+            public int max act 
+        {
+get{return max act;
+      }
+            set{ max act value;
+                
+          }
+        public int salary 
+
+}         
+    }
+using  System;
+    using system . windows.forms;
+    class pointest
+    {
+                static void Main(string[] args)
+        {
+                    employee = new employee();
+                }
+    }
+        }
+  
