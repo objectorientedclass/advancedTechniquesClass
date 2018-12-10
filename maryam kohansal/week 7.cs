8ab05ff62a237e383ea8773834e1ce7c3c898b44
mahdihadian <mahdihadian@hotmail.com>
2018-12-10 09:37:08 +0330

    19azar 9:37AM

---
+++ maryam kohansal/week 7.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _19
+{
+   using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _18_اذر
+{
+    class employyee
+
+        public class date:employee
+        {
+            private string firstname;
+            private string lastname;
+           private string int birthday;
+            private int deportman code;
+            //contructor
+            public Employee(string firstName ValueType, string LastName Value,int birthday, int deportmancode)
+        {
+            FirstName=firstNameValue;
+            LastName= lastNameValue;
+            BirthDay= birthday
+            DeportmanCode=deportmancod
+
+        }
+        //property FirstName
+        public string FirstName
+       {
+        get
+       {
+    return firstName;
+}
+    set
+{
+    firstName=Value;
+}
+}
+    //property LastName
+    public string LastName
+{
+    get
+{
+    return lastName;
+}
+    set
+{
+    lastName = Value;
+}
+     public int Birthday;
+{
+    get
+{
+    return birthday;
+}
+    set
+{
+    birthday = Value;
+}
+     public int deportmancode
+
+{
+    get
+{
+    return deportmancod;
+}
+    set
+{
+    deportmancod = Value;
+}
+
+}
+    
+}//end class date
+
